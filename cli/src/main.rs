cd082c7871a6f3372546e45b6ac655cfee10cd7e
Sergei Kharchikov <callmestech@gmail.com>
2025-12-27 01:00:23 +0000

    add ratatui

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use color_eyre::eyre::Context;
+use color_eyre::Result;
+use crossterm::event::{self, KeyCode};
+use ratatui::widgets::Paragraph;
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> Result<()> {
+    color_eyre::install()?; // augment errors / panics with easy to read messages
+    ratatui::run(run).context("failed to run app")
+}
+
+fn run(terminal: &mut DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if should_quit()? {
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame) {
+    let greeting = Paragraph::new("Hello World! (press 'q' to quit)");
+    frame.render_widget(greeting, frame.area());
+}
+
+fn should_quit() -> Result<bool> {
+    if event::poll(Duration::from_millis(250)).context("event poll failed")? {
+        let q_pressed = event::read()
+            .context("event read failed")?
+            .as_key_press_event()
+            .is_some_and(|key| key.code == KeyCode::Char('q'));
+        return Ok(q_pressed);
+    }
+    Ok(false)
 }
